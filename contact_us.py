3b0c78153d45958671ea19964a3632a019db61b8
Amikul <36567343+amikul@users.noreply.github.com>
2019-03-25 20:38:26 +0000

    Modified contact_us.py

--- contact_us.py
+++ contact_us.py
@@ -6,17 +6,10 @@ from flask import Flask, render_template, request
 
 app = Flask("MyApp")
 
-@app.route("/contact-us")
-def hello():
-    return render_template("contact_us.html")
-
-def send_simple_message():
-    return requests.post(
-        "https://api.mailgun.net/v3/sandboxae7220a00ff84c7ca7fe2dc6f566a5ed.mailgun.org/messages",
-        auth=("api", "a9b68955e996430e41e4303d25eb50cd-de7062c6-302dc37f"),
-        data={"from": "Excited User <mailgun@sandboxae7220a00ff84c7ca7fe2dc6f566a5ed.mailgun.org>",
-            "to": ["cathy7oranges@gmail.com"],
-            "subject": "Helloooooooooooooooo",
-            "text": "Testing some Mailgun awesomness!"})
+@app.route("/contact-us, methods=[POST]")
+def sign_ups():
+    form_data = request.form
+    print form_dats["email"]
+    return "All OK"
 
 app.run(debug=True)
