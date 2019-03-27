1ef67430333dc1dbc9a4c7053865d8b49b56eb3b
KexinQue <kq229@bath.ac.uk>
2019-03-27 14:45:34 +0000

    update index.py

--- index.py
+++ index.py
@@ -17,22 +17,23 @@ def contact_page():
 
 # Post form when submit button is clicked
 @app.route("/contact_us", methods=["POST"])
+
 def contact_us():
     form_data = request.form
     print (form_data["email"])
-    return "All OK"
+    send_simple_message()
+    return "all ok"
 
-# To Cathy: Modify contact_us function with the mailgun API.
 
 def send_simple_message():
-	return requests.post(
-		"https://api.mailgun.net/v3/sandboxae7220a00ff84c7ca7fe2dc6f566a5ed.mailgun.org/messages",
-		auth=("api", "a9b68955e996430e41e4303d25eb50cd-de7062c6-302dc37f"),
-		data={"from": "Excited User <mailgun@sandboxae7220a00ff84c7ca7fe2dc6f566a5ed.mailgun.org>",
-			"to": [form_data["email"]],
-			"subject": "Helloooooooooooooooo",
-			"text": "Testing some Mailgun awesomness!"})
-
+    form_data = request.form
+    return requests.post(
+    "https://api.mailgun.net/v3/sandboxae7220a00ff84c7ca7fe2dc6f566a5ed.mailgun.org/messages",
+    auth=("api", "a9b68955e996430e41e4303d25eb50cd-de7062c6-302dc37f"),
+    data={"from": "Excited User <mailgun@sandboxae7220a00ff84c7ca7fe2dc6f566a5ed.mailgun.org>",
+          "to": form_data["email"],
+          "subject": "Helloooooooooooooooo",
+          "text": "Testing some Mailgun awesomness!"})
 
 # Run app if application is debugged
 app.run(debug=True)
