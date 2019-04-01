c12a3ed2f37ba12db357d48be64017d447a6f5db
Amikul <36567343+amikul@users.noreply.github.com>
2019-04-01 19:01:39 +0100

    Recipe Data function
    Added recipe data function into index.pu

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 # Import requests
 import requests
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
 # Import Flask
 from flask import Flask, render_template, request
 
@@ -48,3 +50,29 @@ def thanks_page():
 # Run app if application is debugged
 app.run(debug=True)
 app.run(port = 5000)
+
+def getRecipeByIngredients(ingredients):
+    payload = {
+        'fillIngredients': False,
+        'ingredients': ingredients,
+        'limitLicense': False,
+        #'number': 5,
+        #'ranking': 1
+    }
+
+    api_key = "c7135f2f8bmsh9b6b78982ba931fp135282jsn85ad20e4d037"
+
+    endpoint = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients"
+
+
+    headers={
+        "X-Mashape-Key": "c7135f2f8bmsh9b6b78982ba931fp135282jsn85ad20e4d037",
+
+    }
+
+    r = requests.get(endpoint, params=payload, headers=headers)
+    results = r.json()
+    i = 0
+    for i in range(len(results)):
+        title = results[i]['title']
+        print(title)
