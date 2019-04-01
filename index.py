c54bb71364305241e391cb491b0eabced37cf027
Amikul <36567343+amikul@users.noreply.github.com>
2019-04-01 18:23:31 +0100

    Update index.py
    Adding route to recipe pages

--- index.py
+++ index.py
@@ -10,6 +10,11 @@ app = Flask("MyApp")
 def hello():
     return render_template("index.html")
 
+# load recipe page
+@app.route("/<dish>")
+def recipepage(dish):
+    return render_template("dish.html" )
+
 # Load Contact Us page
 @app.route("/contact_us")
 def contact_page():
